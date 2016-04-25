a7033eed935ea13dc2e754eb04f9ed11961c5c61
webtalk <webtalk445@gmail.com>
2016-04-25 20:07:36 +0530

    updatd mongodb direct url

--- mongoUtil.js
+++ mongoUtil.js
@@ -3,7 +3,8 @@ var client = mongo.MongoClient;
 var _db;
 
 exports.connect = function(){
-  var db_connect_url = process.env.MONGODB_URI || 'mongodb://localhost:27017/giveback';
+  //var db_connect_url = process.env.MONGODB_URI || 'mongodb://localhost:27017/giveback';
+  var db_connect_url = 'mongodb://admin:admin@ds019481.mlab.com:19481/giveback';
     //client.connect('mongodb://localhost:27017/giveback', function(err, db) {
     client.connect(db_connect_url, function(err, db) {
       if(err) {
@@ -11,7 +12,7 @@ exports.connect = function(){
         process.exit(1);
       }
       _db = db;
-      console.log("Connected to Mongo");
+      console.log("Connected to Mongo url -->"+db_connect_url);
     });
   }
 
